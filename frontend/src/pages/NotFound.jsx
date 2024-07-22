d8a1508937f22dc26ec58f0c77801b9d74ab581b
jensoncaparida <jensoncaparida@gmail.com>
2024-07-23 00:57:13 +0800

    initial commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div>
+      <h1>404 NotFound</h1>
+      <p>The page you are looking for is not might exist.</p>
+    </div>
+  );
+}
+
+export default NotFound;
